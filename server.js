ffdad8ba20ae4c45cbe9031cb232724cd8f06cfd
Ken Miyamoto <kmiyamoto13@gmail.com>
2016-02-08 18:08:26 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,39 @@
+// get all the tools we need
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 3000;
+var mongoose = require('mongoose');
+var passport = require('passport');
+
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
+var configDB = require('./config/database.js');
+
+// configuration ===============================================================
+mongoose.connect(configDB.url); // connect to our database
+
+require('./config/passport')(passport); // pass passport for configuration
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.json()); // get information from html forms
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// required for passport
+app.use(session({ secret: 'session-secret' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
+// routes ======================================================================
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+// launch ======================================================================
+app.listen(port);
+console.log('Server listening on port: ' + port);
+
+// export the app object and make it available to other files such as the test file
+module.exports = app;
